7ce3a46a29b7e1297529e9680137190a3ba9f3c2
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-01-31 22:49:06 +0900

    WIP - clicl left/right cahnge image

---
+++ source/assets/javascripts/all.js
@@ -0,0 +1,41 @@
+/*
+
+*= require jquery.min
+*= require bootstrap
+
+*/
+
+$(document).ready(function() {
+
+    var currentImage = 0;
+    var imgArray = $("#photodisplay img").map(function() {
+        return $(this).attr("src");
+    });
+
+
+    var changeBG = function(index) {
+        var bg = "url(" + imgArray[index] + ")";
+        $('#photodisplay').fadeOut('slow',
+                                   function() {
+                                       $(this).css("background-image",
+                                                   bg).fadeIn('slow')
+                                   }
+                                  )};
+
+    $('#next').on('click', function(e) {
+        currentImage++;
+        if (currentImage > imgArray.length -1 ) {
+            currentImage = 0;
+        }
+        changeBG(currentImage);
+    });
+
+
+    $("#prev").click(function() {
+        currentImage--;
+        if (currentImage < 0 ) {
+            currentImage = imgArray.length -1;
+        }
+        changeBG(currentImage);
+    });
+});
